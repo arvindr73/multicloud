2a3f2f44f20016cbd3249528fcecd6606bc71044
arvindr73 <66772823+arvindr73@users.noreply.github.com>
2020-06-13 15:37:12 +0530

    commit 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<pre>
+<?php
+ print " Welcome t my first webpage developed using Terraform "
+ print `date`
+ print "\n"
+ print "\n"
+ print "\n"
+ print "\n"
+ print "Copy right @2020 "
+ 
+?>
+</pre>
